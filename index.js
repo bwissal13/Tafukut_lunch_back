f26e918b23568b064a2bd538ef61c982fe226c02
bwissal <baazizwissal13@gmail.com>
2024-07-10 13:19:50 +0100

    update

--- index.js
+++ index.js
@@ -39,7 +39,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://tafukut-lunch-back.vercel.app/',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 app.use(bodyParser.json());
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
