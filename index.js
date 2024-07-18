891d1e80cc8a0bc044b88437f8820408e05295be
bwissal <baazizwissal13@gmail.com>
2024-07-18 11:42:58 +0100

    update

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -6,50 +5,30 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 
 dotenv.config();
-// Middleware to parse JSON
-app.use(express.json());
 
-// CORS Middleware
-app.use(function (req, res, next) {
-  // Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-  next();
-});
-// const app = express();
-
-// const allowedOrigins = ['https://tafukut-lunch.vercel.app'];
-
-// app.use(cors({
-//   origin: function (origin, callback) {
-//     console.log(`Origin: ${origin}`);
-//     if (!origin) return callback(null, true);
-//     if (allowedOrigins.indexOf(origin) === -1) {
-//       const msg = 'khdouj';
-//       console.log(msg);
-//       return callback(new Error(msg), false);
-//     }
-//     return callback(null, true);
-//   },
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization'],
-//   credentials: true,
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204
-// }));
-// app.use(bodyParser.json());
+const app = express();
+const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGODB_URI);
 
 const waitlistSchema = new mongoose.Schema({
   name: String,
-  email: String
+  email: String,
 });
 
 const Waitlist = mongoose.model('Waitlist', waitlistSchema);
 
-app.options('*', cors()); 
+app.use(express.json());
+app.use(cors({
+  origin: 'https://tafukut-lunch.vercel.app', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 204,
+}));
+
+app.options('*', cors());
+
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Server is running' });
 });
@@ -60,8 +39,6 @@ app.post('/api/waitlist', async (req, res) => {
   await newEntry.save();
   res.json({ text: `Thank you, ${name}! You have been added to the waitlist with the email: ${email}` });
 });
-
-const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
