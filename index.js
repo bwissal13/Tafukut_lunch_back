de2908e2f1a31acbfafda023fde8467ac11eaf8e
bwissal <baazizwissal13@gmail.com>
2024-07-18 12:11:58 +0100

    update

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 }).then(() => {
   console.log('Connected to MongoDB');
 }).catch(err => {
