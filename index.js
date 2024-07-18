bed553a4cabc58812c39c6c7935b4454e4f2c78a
bwissal <baazizwissal13@gmail.com>
2024-07-18 12:11:31 +0100

    update

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
+const cors = require('cors');
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// MongoDB Connection
 mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 }).then(() => {
   console.log('Connected to MongoDB');
 }).catch(err => {
   console.error('Error connecting to MongoDB:', err);
 });
 
-// Define schema and model
 const waitlistSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
@@ -23,21 +23,19 @@ const waitlistSchema = new mongoose.Schema({
 
 const Waitlist = mongoose.model('Waitlist', waitlistSchema);
 
-// Middleware
 app.use(express.json());
-
 app.use(cors({
   origin: 'https://tafukut-lunch.vercel.app', 
   methods: ['GET', 'HEAD', 'OPTIONS', 'POST', 'PUT'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
 }));
 
-// Routes
-app.get('/api/test', allowCors((req, res) => {
-  res.json({ message: 'Server is running' });
-}));
 
-app.post('/api/waitlist', allowCors(async (req, res) => {
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Server is running' });
+});
+
+app.post('/api/waitlist', async (req, res) => {
   try {
     const { name, email } = req.body;
     const newEntry = new Waitlist({ name, email });
@@ -46,9 +44,9 @@ app.post('/api/waitlist', allowCors(async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: 'An error occurred while adding to the waitlist' });
   }
-}));
+});
 
-// Start the server
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
